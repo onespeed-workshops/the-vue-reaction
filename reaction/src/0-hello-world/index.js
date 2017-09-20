a642cb5d37e20697ea86e1304d5d2a1d5c126e7f
PatrickJS <github@gdi2290.com>
2017-09-19 21:30:39 -0700

    hello

---
+++ reaction/src/0-hello-world/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          Hello World
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
